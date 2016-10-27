3782e516c569c2521880494b1b7035edf01134bc
wfeiring <wfeiring@gmail.com>
2016-10-27 15:39:00 +0200

    Add files via upload

---
+++ motob.py
@@ -0,0 +1,12 @@
+
+class motob:
+
+    def __init__(self, motor, motor2):
+        motors = [motor, motor2]
+        value = []
+
+    def update(self, motor_rec):
+        self.value = motor_rec
+
+    def operationalize(self):
+        pass
