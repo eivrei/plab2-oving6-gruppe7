00152d07ca828a132107caf47fd9610ff371f436
Molly <malfridha@gmail.com>
2016-11-02 13:14:13 +0100

    Gjort nokså lite

--- arbitrator.py
+++ arbitrator.py
@@ -1,7 +1,14 @@
+import random
+
 class Arbitrator:
     def __init__(self, bbcon):
         self.bbcon = bbcon
         self.stochastic = None
+
+    # def pick_behavior(self):
+    #     weights = 0
+    #     weights += self.bbcon.weight
+    #     nr = random.randrange(weights)
 
     # Check active_behaviors and pick the winner
     # Return tuple with motor recommendations and a boolean indicating whether the run should be halted or not
