50cf2aff0d1c3b87a7ab0dc71285c7fdfa09e214
Molly <malfridha@gmail.com>
2016-11-02 12:40:48 +0100

    Oppdatert og lagt til metoder

--- Sensob.py
+++ Sensob.py
@@ -3,15 +3,27 @@ class Sensob:
 
     def __init__(self):
         self.sensor_list = []
+        self.value_list = []
+
 
     def update(self):
-        raise NotImplementedError
+        for sensor in self.sensor_list:
+            sensor.update()
+            get_valuelist.append(sensor.get_value())
 
-    def get_value(self):
-        raise NotImplementedError
 
-    def reset(self):
-        raise NotImplementedError
+    def reset_sensors(self):
+        for sensor in sensor_list:
+            sensor.reset()
+
 
     def add_sensor(self, sensor):
-        raise NotImplementedError
+        get_sensorlist.append(sensor)
+
+
+    def get_sensorlist(self): #unødvendig?
+        return self.sensor_list
+
+
+    def get_valuelist(self): #unøvendig?
+        return self.get_valuelist
