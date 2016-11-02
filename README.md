da8d85094bf35ba7583e090fba677425e4550bd6
wfeiring <wfeiring@gmail.com>
2016-11-02 11:53:27 +0100

    Lagt til strategi

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # plab2-oving6-gruppe7
+IP: 129.241.162.97
+MAC: b8:27:eb:c6:03:8d
+DNS: rpi-162-97
+
+
+
+Behaviors:
+    - Avoid front collision
+        - Pri = 1
+        - Sensob:
+
+    - Avoid side collision
+        - Pri = 2
+
+    - Search for colour
+        - Pri = 3
+
+    - Follow line
+        - Pri = 4
---
+++ bbcon.py
@@ -0,0 +1,55 @@
+
+class Bbcon:
+    def __init__(self):
+        self.behaviors = set()
+        self.active_behaviors = set()
+        self.sensobs = set()
+        self.motobs = set()
+        self.arbitrator = None
+
+    # Add behavior to self.behaviors
+    def add_behavior(self, behavior):
+        self.behaviors.add(behavior)
+
+    # Add sensob to self.sensobs
+    def add_sensob(self, sensob):
+        self.sensobs.add(sensob)
+
+    # Add behavior to active_behaviors
+    def activate_behavior(self, behavior):
+        self.active_behaviors.add(behavior)
+
+    # Remove behavior from active_behaviors
+    def deactivate_behavior(self, behavior):
+        self.active_behaviors.remove(behavior)
+
+    # Return if behavior is active
+    def behavior_is_active(self, behavior):
+        return behavior in self.active_behaviors
+
+    # Main function of the bbcon object. Does all the heavy lifting
+    def run_one_timestep(self):
+        # Run update method in all sensob objects
+        def update_sensobs():
+            for sensob in self.sensobs:
+                sensob.update()
+
+        # Run update method in all behavior objects
+        def update_behaviors():
+            for behavior in self.behaviors:
+                behavior.update()
+
+        # Invoke the arbitrator with arbitrator.choose_action
+        # To be implemented...
+
+        # Run update method in all motob objects
+        def update_motobs():
+            pass
+
+        # Wait while robot is moving for a short period
+        # To be implemented...
+
+        # Run reset method in all sensob objects
+        def reset_sensobs():
+            for sensob in self.sensobs:
+                sensob.reset()
