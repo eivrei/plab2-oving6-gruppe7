1dadc14dae108ee4eaa9f545c3e58e44425263a0
Molly <malfridha@gmail.com>
2016-11-04 10:54:34 +0100

    Removed a method

--- arbitrator.py
+++ arbitrator.py
@@ -1,17 +1,17 @@
-import random
+
 
 class Arbitrator:
+
     def __init__(self, bbcon):
         self.bbcon = bbcon
         self.stochastic = None
 
-    # def pick_behavior(self):
-    #     weights = 0
-    #     weights += self.bbcon.weight
-    #     nr = random.randrange(weights)
 
-    # Check active_behaviors and pick the winner
-    # Return tuple with motor recommendations and a boolean indicating whether the run should be halted or not
     def choose_action(self):
-        pass
+        # Check active_behaviors
+        # Pick the winner:
+        # winner_weight = max(bbcon.weight_list)
+        return [motor_recommendations, bool] # <- wether the run should be halted or not
+
+
 
