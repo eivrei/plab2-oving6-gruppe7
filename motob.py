8806eaa02432716b0d79966c09f6fe6b81940c4c
wfeiring <wfeiring@gmail.com>
2016-11-04 11:21:33 +0100

    update methods sligthy

--- motob.py
+++ motob.py
@@ -13,17 +13,8 @@ class motob:
 
     def operationalize(self):
         if self.value[0] == "B":
-            if self.value[1] == 180:
                 self.motor.backward(0.5, 1)
                 self.motor.left(0.5, 1.5)
-
-            elif self.value[1] == 90:
-                self.motor.backward(0.5, 1)
-                self.motor.left(0.5, 0.75)
-
-            elif self.value[1] == 45:
-                self.motor.backward(0.5, 1)
-                self.motor.left(0.5, 0.375)
 
         elif self.value[0] == "L":
             if self.value[1] == 180:
@@ -40,10 +31,14 @@ class motob:
                 self.motor.right(0.5, 1.5)
 
             elif self.value[1] == 90:
-                self.motor.right(0.5, 90)x
+                self.motor.right(0.5, 90)
 
             elif self.value[1] == 45:
                 self.motor.right(0.5, 0.375)
 
         elif self.value[0] == "F":
             self.motor.forward(0.5, 1)
+
+
+# @ 50% 360 degree turn takes 3 sec
+# 10 degree turn @ 50 % takes 3/360 * 10
