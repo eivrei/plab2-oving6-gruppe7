3e911989ee0c7c5c49bf8247a6f59307b30c41b1
eivrei <22083159+eivrei@users.noreply.github.com>
2016-11-07 09:34:50 +0100

    Add degree variable and fix code

--- motob.py
+++ motob.py
@@ -5,38 +5,23 @@ class Motob:
     def __init__(self):
         self.motor = Motors()
         self.value = []
+        # @ 50% speed, 360 degree turn takes 3 sec.
+        # 10 degree turn @ 50 % takes 3/360 * 10
+        self.time_per_degree = round(3 / 360, 4)
 
     def update(self, motor_rec):
         self.value.append(motor_rec)
 
     def operationalize(self):
         if self.value[0] == "B":
-                self.motor.backward(0.5, 1)
-                self.motor.left(0.5, 1.5)
+            self.motor.backward(0.5, 1)
+            self.motor.left(0.5, self.time_per_degree * 180)
 
         elif self.value[0] == "L":
-            if self.value[1] == 180:
-                self.motor.left(0.5, 1.5)
-
-            elif self.value[1] == 90:
-                self.motor.left(0.5, 0.75)
-
-            elif self.value[1] == 45:
-                self.motor.left(0.5, 0.375)
+            self.motor.left(0.5, self.time_per_degree * self.value[1])
 
         elif self.value[0] == "R":
-            if self.value[1] == 180:
-                self.motor.right(0.5, 1.5)
-
-            elif self.value[1] == 90:
-                self.motor.right(0.5, 90)
-
-            elif self.value[1] == 45:
-                self.motor.right(0.5, 0.375)
+            self.motor.right(0.5, self.time_per_degree * self.value[1])
 
         elif self.value[0] == "F":
             self.motor.forward(0.5, 1)
-
-
-# @ 50% 360 degree turn takes 3 sec
-# 10 degree turn @ 50 % takes 3/360 * 10
